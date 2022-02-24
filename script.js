a57bf8963ace4d757727ed5ace140356d475b852
Peyton Duong <peytongd468@gmail.com>
2022-02-23 23:15:13 -0600

    script updates

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ var pokeImage = document.getElementById('randomPoke')
 function fetchPokemom() {
     var pokeNum = Math.floor(Math.random() * 898) + 1; //variable for number of Pokemon
     console.log(pokeNum)
-    fetch('https://pokeapi.co/api/v2/pokemon/1')
+    fetch(`https://pokeapi.co/api/v2/pokemon/1`)
     .then( res => {
         return res.json
     })
-    /*.then( data => {
+    .then( (data) => {
         console.log(data);
-        const pokemon = results.map((data) => {
+        const pokemon = {
             name: data.name,
             id: data.id,
             image: data.sprites['front_default']
-        });
-        document.getElementById("randomPoke").src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png";
-    })*/
+        };
+        
+    })
     
 }
 
