3daef115eaf63859e1e1637ae794318f52559e88
Peyton Duong <peytongd468@gmail.com>
2022-02-24 10:34:56 -0600

    random but with api

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-var pokeImage = document.getElementById('randomPoke')
+var pokeNum = Math.floor(Math.random() * 898) + 1; //variable for number of Pokemon
 
 function fetchPokemom() {
-    var pokeNum = Math.floor(Math.random() * 898) + 1; //variable for number of Pokemon
     console.log(pokeNum)
-    fetch(`https://pokeapi.co/api/v2/pokemon/1`)
-    .then( res => {
-        return res.json
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`)
+    .then( (res) => {
+        return res.json();
     })
     .then( (data) => {
         console.log(data);
@@ -14,23 +13,41 @@ function fetchPokemom() {
             id: data.id,
             image: data.sprites['front_default']
         };
-        
+        console.log(pokemon.name);
+        document.getElementById("randomPoke").style.filter="brightness(0%)";
+        var url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeNum}.png`;
+        document.getElementById("randomPoke").src=`${url}`;
+        console.log(url);
+        console.log(document.getElementById(`guess`).value);
+        console.log(JSON.stringify(pokemon.name));
+        if ((JSON.stringify(document.getElementById('guess').value) === "a")){
+            console.log("works");
+        }
     })
-    
 }
 
 function randomId(){
-    document.getElementById("randomPoke").style.filter="brightness(0%)";
+    pokeNum = Math.floor(Math.random() * 898) + 1; 
+    fetchPokemom();
+    /*document.getElementById("randomPoke").style.filter="brightness(0%)";
     var url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${Math.floor(Math.random() * 898) + 1}.png`;
     document.getElementById("randomPoke").src=`${url}`;
     console.log(url);
-    
+    */
 }
 
 function show(){
     document.getElementById("randomPoke").style.filter="brightness(100%)";
 }
 
+function guess(poke){
+    if (document.getElementById(`guess`).value === poke){
+        console.log(works);
+    }
+}
+
 randomId();
 document.getElementById("showButton").addEventListener("click", show);
 document.getElementById("randButton").addEventListener("click", randomId);
+document.getElementById("guessButton").addEventListener("click", guess);
+fetchPokemom();
