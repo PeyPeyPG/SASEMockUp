434c8b6669c49ec92ecae1c2ae14e640750ed6d2
Peyton Duong <peytongd468@gmail.com>
2022-02-24 21:50:18 -0600

    complete wtp game

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 var pokeNum = Math.floor(Math.random() * 898) + 1; //variable for number of Pokemon
+var giveUpBool = false;
 
 function fetchPokemom() {
-    console.log(pokeNum)
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`)
     .then( (res) => {
         return res.json();
     })
     .then( (data) => {
-        console.log(data);
         const pokemon = {
             name: data.name,
             id: data.id,
             image: data.sprites['front_default']
         };
-        console.log(pokemon.name);
         document.getElementById("randomPoke").style.filter="brightness(0%)";
         var url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeNum}.png`;
         document.getElementById("randomPoke").src=`${url}`;
-        console.log(url);
+        var pokeStr = JSON.stringify(document.getElementById(`guess`).value);
         console.log(document.getElementById(`guess`).value);
         console.log(JSON.stringify(pokemon.name));
-        if ((JSON.stringify(document.getElementById('guess').value) === "a")){
-            console.log("works");
+        if (giveUpBool) {
+            show();
+            document.getElementById(`guess`).value = pokemon.name;
+        }
+        if (pokeStr === JSON.stringify(pokemon.name)){
+            show();
+            document.getElementById(`guessButton`).style.backgroundColor = "lime";
+            document.getElementById(`guessButton`).style.color = "white";
+            document.getElementById(`guessButton`).innerHTML = "CORRECT";
+            document.getElementById(`giveButton`).innerHTML = "TRY AGAIN";
+        }
+        else if (pokeStr.length > 2){
+            console.log(JSON.stringify(document.getElementById(`guess`).value).length)
+            document.getElementById(`guessButton`).style.backgroundColor = "red";
+            document.getElementById(`guessButton`).style.color = "white";
+            document.getElementById(`guessButton`).innerHTML = "TRY AGAIN";
+            document.getElementById(`guess`).value = "";
         }
     })
 }
@@ -29,25 +42,44 @@ function fetchPokemom() {
 function randomId(){
     pokeNum = Math.floor(Math.random() * 898) + 1; 
     fetchPokemom();
-    /*document.getElementById("randomPoke").style.filter="brightness(0%)";
-    var url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${Math.floor(Math.random() * 898) + 1}.png`;
-    document.getElementById("randomPoke").src=`${url}`;
-    console.log(url);
-    */
+}
+
+function hide(){
+    document.getElementById("randomPoke").style.filter="brightness(0%)";
 }
 
 function show(){
     document.getElementById("randomPoke").style.filter="brightness(100%)";
 }
 
-function guess(poke){
-    if (document.getElementById(`guess`).value === poke){
-        console.log(works);
+function guessAgain(){
+    document.getElementById(`guess`).value = "";
+    document.getElementById(`guessButton`).innerHTML = "GUESS";
+    document.getElementById(`guessButton`).style.color = "black";
+    document.getElementById(`guessButton`).style.backgroundColor = "white";
+}
+
+function giveUp(){
+    if (document.getElementById(`giveButton`).innerHTML === "GIVE UP"){
+        show();
+        document.getElementById(`giveButton`).innerHTML = "TRY AGAIN";
+        giveUpBool = true;
+        fetchPokemom();
+    }
+    else if (document.getElementById(`giveButton`).innerHTML === "TRY AGAIN"){
+        document.getElementById(`giveButton`).innerHTML = "GIVE UP";
+        giveUpBool = false;
+        hide();
+        randomId();
+        guessAgain();
     }
 }
 
+function guess(){
+    fetchPokemom();
+}
+
 randomId();
-document.getElementById("showButton").addEventListener("click", show);
-document.getElementById("randButton").addEventListener("click", randomId);
+document.getElementById("giveButton").addEventListener("click", giveUp);
 document.getElementById("guessButton").addEventListener("click", guess);
 fetchPokemom();
